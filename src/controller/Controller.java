5cfa0ad19211487c7a979dfd6521f3c2a3a837c1
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-07-01 16:20:41 +0100

    Begin on MVC pattern

---
+++ src/controller/Controller.java
@@ -0,0 +1,10 @@
+package controller;
+
+//
+public interface Controller {
+    /**
+     * Connect to Database and retrieve data
+     * Use for when first loading application
+     */
+    void loadDatabase();
+}
---
+++ src/user_agent/Main.java
@@ -0,0 +1,12 @@
+package user_agent;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HABIT TRACKER");
+        loadDatabase();
+    }
+
+    public static void loadDatabase() {
+
+    }
+}
