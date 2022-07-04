80f7696e040534021c04f46ea081ce90bce2540d
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-07-04 21:52:59 +0100

    Set up GUI Class for graphical view

---
+++ src/view/GraphicalView.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.sql.ResultSet;
+
+public class GraphicalView implements View {
+    @Override
+    public void getData() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void update(ResultSet context) {
+
+    }
+}
