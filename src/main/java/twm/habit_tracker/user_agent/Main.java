6b762d25fedf6cda2d77ba46842a2880d87efe21
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-07-05 19:59:57 +0100

    Final commit for day - removed uneeded imports

--- src/main/java/twm/habit_tracker/user_agent/Main.java
+++ src/main/java/twm/habit_tracker/user_agent/Main.java
@@ -1,9 +1,6 @@
 package twm.habit_tracker.user_agent;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import twm.habit_tracker.controller.ConcreteController;
 import twm.habit_tracker.controller.Controller;
