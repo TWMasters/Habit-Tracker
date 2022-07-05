03785d18b39833e5e11fc0c1e2b479cc6f774a9f
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-07-05 14:39:02 +0100

    Commit before switching over to JavaFX project

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,4 +1,8 @@
+// --module-path "/Users/Toby/Dropbox/04 - Coding/Coding Practise - Projects/javafx-sdk-16/lib" --add-modules javafx.controls,javafx.fxml
+
 package view;
+
+
 
 public class GUI {
 
