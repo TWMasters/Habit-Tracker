9ff555aed892ff81fa9f3a76fa89934d4cb87c4d
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-07-12 17:01:13 +0100

    Set up form fields for adding a habit

--- src/main/java/twm/habit_tracker/view/GUIView.java
+++ src/main/java/twm/habit_tracker/view/GUIView.java
@@ -5,15 +5,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -99,10 +99,31 @@ public class GUIView implements View {
      * @return Pane
      */
     public Pane getHabitPage() {
+        // Layout
+        GridPane layout = new GridPane();
+        layout.setAlignment(Pos.TOP_CENTER);
+        layout.setHgap(10);
+        layout.setVgap(10);
+        layout.setPadding(new Insets(25, 25, 25, 25));
+
         //  Elements
-        Label header = new Label("HABIT PAGE");
+        Text header = new Text("HABIT PAGE");
+        layout.add(header, 1, 0, 2, 1);
+
+        Label habitName = new Label("Habit Name: ");
+        layout.add(habitName, 0, 1);
+        TextField habitNameTF = new TextField();
+        layout.add(habitNameTF, 1, 1, 2, 1);
+
+        Label habitQuestion = new Label("Habit Question: ");
+        layout.add(habitQuestion, 0, 2);
+        TextField habitQuestionTF = new TextField();
+        layout.add(habitQuestionTF, 1, 2, 2, 1);
+
         Button addHabitButton = new Button("Create");
+        layout.add(addHabitButton, 1, 3, 2, 1);
         Button backButton = new Button("Back");
+        layout.add(backButton, 1, 4, 2, 1);
 
         // Wiring
         addHabitButton.setOnAction(createHabitEventHandler);
@@ -111,10 +132,6 @@ public class GUIView implements View {
             window.setScene(homePage);
         });
 
-        // Layout
-        VBox layout  = new VBox(20);
-        layout.getChildren().addAll(header, addHabitButton, backButton);
-        layout.setAlignment(Pos.TOP_CENTER);
         return layout;
     }
 
