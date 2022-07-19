ae693b971402041c065099cbc091daf7ba8c3550
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-07-19 13:01:16 +0100

    Finished tests for HabitState

--- src/main/test/HabitTableStateTest.java
+++ src/main/test/HabitTableStateTest.java
@@ -12,14 +12,15 @@ public class HabitTableStateTest {
     private static final String H2_URL = "jdbc:h2:./db/Habits";
 
     private static Connection connection;
-    private static int[] primaryKeys = new int[3];
+    private static final int[] primaryKeys = new int[3];
     private static Model testModel;
+    private static Statement stmt;
 
     @BeforeAll
     static void setup() {
         try {
             connection = DriverManager.getConnection(H2_URL);
-            Statement stmt = connection.createStatement();
+            stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT MAX(Habit_ID) FROM Habits;");
             rs.next();
             int highestID = rs.getInt(1) + 1;
@@ -31,13 +32,6 @@ public class HabitTableStateTest {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
         testModel = ConcreteModel.getModel();
         testModel.changeTargetTable(new HabitTableState());
@@ -49,8 +43,6 @@ public class HabitTableStateTest {
         testModel.closeConnection();
         // Write SQL to ensure added rows have been deleted
         try {
-            connection = DriverManager.getConnection(H2_URL);
-            Statement stmt = connection.createStatement();
             stmt.execute("DELETE FROM Habits " +
                     "WHERE Habit_ID = " + primaryKeys[0] + ";");
             stmt.execute("DELETE FROM Habits " +
@@ -87,33 +79,55 @@ public class HabitTableStateTest {
 
     }
 
-
-
     @Test
     void testAddRow() {
         String[] input = {"TestHabit2", "true", "Have you done TestHabit2?", "null", "null"};
         try {
             testModel.addEntry(input);
             // testModel.closeConnection();
-            connection = DriverManager.getConnection(H2_URL);
-            Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM Habits WHERE Habit_ID = " + primaryKeys[1] + ";");
             if (rs.isBeforeFirst()) {
                 Assertions.assertEquals("TestHabit2", rs.getString(2));
                 Assertions.assertEquals("Have you done TestHabit2?", rs.getString(4));
             }
             else
-                Assertions.fail("Row not added!");
+                Assertions.fail("Added Row could not be found!");
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testDeleteRow() {
+        try {
+            testModel.deleteEntry(String.valueOf(primaryKeys[0]));
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Habits WHERE Habit_ID = " + primaryKeys[0] + ";");
+            if (rs.isBeforeFirst()) {
+                Assertions.fail("Entry still exists!");
             }
+            else
+                return;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testEditEntry() {
+        try {
+            stmt.execute("INSERT INTO Habits VALUES(" + primaryKeys[2] + ", \'TestHabit3\', true, \'Have you done TestHabit3?\', null, null)");
+            String[] newValues = {"TestHabit3Edited", "true", "Have you done TestHabit3Edited?", "null", "null"};
+            testModel.editEntry(newValues, String.valueOf(primaryKeys[2]));
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Habits WHERE Habit_ID = " + primaryKeys[2] + ";");
+            rs.next();
+            Assertions.assertEquals("TestHabit3Edited", rs.getString(2));
+            Assertions.assertEquals("Have you done TestHabit3?Edited?", rs.getString(4));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
