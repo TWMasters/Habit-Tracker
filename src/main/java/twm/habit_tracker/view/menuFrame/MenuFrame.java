96e413e5cd0a70922a9494a23e51923dda4e49b6
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-08-01 19:40:32 +0100

    Add very simple test functionality to menu frame buttons

--- src/main/java/twm/habit_tracker/view/menuFrame/MenuFrame.java
+++ src/main/java/twm/habit_tracker/view/menuFrame/MenuFrame.java
@@ -1,4 +1,13 @@
 package twm.habit_tracker.view.menuFrame;
 
 public class MenuFrame {
+    public void goalButtonPush() {
+        System.out.println("Navigate to goal page");
+    }
+
+    public void habitButtonPush() {
+        System.out.println("Navigate to habit page");
+    }
+
+
 }
