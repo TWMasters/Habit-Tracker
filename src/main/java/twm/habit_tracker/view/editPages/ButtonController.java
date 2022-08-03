a75ff3360612aa1b1f413ba3e6cbd189535fd0ce
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-08-03 19:10:38 +0100

    Added functionality to back button

--- src/main/java/twm/habit_tracker/view/editPages/ButtonController.java
+++ src/main/java/twm/habit_tracker/view/editPages/ButtonController.java
@@ -1,4 +1,15 @@
 package twm.habit_tracker.view.editPages;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ButtonBar;
+import javafx.stage.Stage;
+
 public class ButtonController {
+    @FXML
+    ButtonBar buttonBarReference;
+
+    public void backButtonPush() {
+        Stage currentWindow = (Stage) buttonBarReference.getScene().getWindow();
+        currentWindow.close();
+    }
 }
