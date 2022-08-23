4c1d2a1191abaf510bddd9a8c26d0d568fbc2789
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-08-23 21:58:41 +0100

    Add start date column to trophy

--- src/main/java/twm/habit_tracker/model/TrophyTable.java
+++ src/main/java/twm/habit_tracker/model/TrophyTable.java
@@ -3,6 +3,8 @@ package twm.habit_tracker.model;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 /**
  * Helper Class for Concrete Model to create static Trophy Table
@@ -12,29 +14,33 @@ class TrophyTable {
             "CREATE TABLE Trophies (\n" +
                     "Trophy_ID VARCHAR(255) PRIMARY KEY,\n" +
                     "Trophy_Won BOOLEAN DEFAULT false NOT NULL,\n" +
+                    "Start_Date DATE NOT NULL,\n" +
                     "Trophy_Message VARCHAR(255) NOT NULL\n" +
                     ");";
 
     private static final String POPULATE_TROPHY_TABLE =
-            "INSERT INTO Trophies (Trophy_ID, Trophy_Message)\n" +
+            "INSERT INTO Trophies (Trophy_ID, Start_Date, Trophy_Message)\n" +
                     "VALUES\n" +
-                    "(\'HalfDay\', \'You Completed Half of your Daily Habits\')," +
-                    "(\'FullDay\', \'You Completed All of your Daily Habits\')," +
-                    "(\'ThreeDay\', \'You Completed your Daily Habits for Three Days this Week\')," +
-                    "(\'FiveDay\', \'You Completed your Daily Habits for Five Days this Week\')," +
-                    "(\'FullWeek\', \'You Completed your Daily Habits for the Week\')," +
-                    "(\'TenDay\',\'You Completed your Daily Habits for Ten Days this Month\')," +
-                    "(\'TwentyDay\', \'You Completed your Daily Habits for Twenty Days this Month\')," +
-                    "(\'FullMonth\', \'You Completed your Daily Habits for Thirty Days this Month\');";
+                    "(\'HalfDay\', \'%s\', \'You Completed Half of your Daily Habits\')," +
+                    "(\'FullDay\', \'%s\', \'You Completed All of your Daily Habits\')," +
+                    "(\'ThreeDay\', \'%s\', \'You Completed your Daily Habits for Three Days this Week\')," +
+                    "(\'FiveDay\', \'%s\', \'You Completed your Daily Habits for Five Days this Week\')," +
+                    "(\'FullWeek\', \'%s\', \'You Completed your Daily Habits for the Week\')," +
+                    "(\'TenDay\', \'%s\', \'You Completed your Daily Habits for Ten Days this Month\')," +
+                    "(\'TwentyDay\', \'%s\', \'You Completed your Daily Habits for Twenty Days this Month\')," +
+                    "(\'FullMonth\', \'%s\', \'You Completed your Daily Habits for Thirty Days this Month\');";
 
-    
+
 
     public static void createTrophyTable(Connection connection)  {
         try {
             Statement stmt = connection.createStatement();
             stmt.execute(TROPHY_TABLE_SQL);
-            stmt.execute(POPULATE_TROPHY_TABLE);
-
+            String today = LocalDate.now().toString();
+            String firstOfWeek = LocalDate.now().with(DayOfWeek.MONDAY).toString();
+            String firstOfMonth = LocalDate.now().withDayOfMonth(1).toString();
+            String populate = String.format(POPULATE_TROPHY_TABLE, today, today, firstOfWeek, firstOfWeek, firstOfWeek, firstOfMonth, firstOfMonth, firstOfMonth);
+            stmt.execute(populate);
 
         } catch (SQLException e) {
             System.err.println("Error on creating Trophy Table");
