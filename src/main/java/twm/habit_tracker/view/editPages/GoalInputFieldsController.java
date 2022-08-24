51bfc2dcffc19495c58b06cb49b2e87c4ba4a4e5
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-08-24 16:16:47 +0100

    Populate year choicebox

--- src/main/java/twm/habit_tracker/view/editPages/GoalInputFieldsController.java
+++ src/main/java/twm/habit_tracker/view/editPages/GoalInputFieldsController.java
@@ -1,14 +1,43 @@
 package twm.habit_tracker.view.editPages;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 public class GoalInputFieldsController extends InputAbstractController{
+
+    private static final int FIFTY_YEARS = 50;
+    private static final int START_YEAR = 2022;
+
+
     private static Runnable targetGoalTable;
 
+
+    @FXML ComboBox dayBox;
+    @FXML ComboBox monthBox;
+    @FXML ComboBox yearBox;
     @FXML TextField goalInput;
     @FXML TextField descriptionInput;
     @FXML TextField deadlineInput;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+        // Year
+        int endYear = LocalDate.now().getYear() + FIFTY_YEARS;
+        ArrayList<String> years = new ArrayList<>();
+        for (int i = START_YEAR; i <= endYear; i++)
+            years.add(String.valueOf(i));
+        yearBox.setItems(FXCollections.observableList(years));
+        // Month
+    }
+
 
     @Override
     public String[] getFields() {
