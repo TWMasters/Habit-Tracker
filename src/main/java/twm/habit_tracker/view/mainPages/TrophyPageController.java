31681100f05d9bb68671fb06b28d7678a89efb51
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-08-26 16:35:27 +0100

    Get trophy  page to write today's date

--- src/main/java/twm/habit_tracker/view/mainPages/TrophyPageController.java
+++ src/main/java/twm/habit_tracker/view/mainPages/TrophyPageController.java
@@ -1,4 +1,29 @@
 package twm.habit_tracker.view.mainPages;
 
-public class TrophyPageController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ResourceBundle;
+
+public class TrophyPageController implements Initializable {
+    @FXML
+    Label dayLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String day = String.valueOf(LocalDate.now().getDayOfMonth());
+        String month = LocalDate.now().getMonth().name();
+        String year = String.valueOf(LocalDate.now().getYear());
+        dayLabel.setText(String.format("%s %s %s", day, month, year));
+    }
+
+    private String makeOrdinal(String input) {
+        int index = input.length() - 1;
+        input = input.substring(index);
+        return null;
+    }
 }
