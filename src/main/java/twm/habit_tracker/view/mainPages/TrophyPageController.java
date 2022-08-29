46a5132b5a6fad4f320fbc031baf88ad36360119
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-08-29 19:38:43 +0100

    Completed draft of trophy page fxml

--- src/main/java/twm/habit_tracker/view/mainPages/TrophyPageController.java
+++ src/main/java/twm/habit_tracker/view/mainPages/TrophyPageController.java
@@ -6,17 +6,32 @@ import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class TrophyPageController implements Initializable {
-    @FXML
-    Label dayLabel;
+    @FXML Label dayLabel;
+    @FXML Label monthLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         String day = String.valueOf(LocalDate.now().getDayOfMonth());
         String month = LocalDate.now().getMonth().name();
         String year = String.valueOf(LocalDate.now().getYear());
+        switch (LocalDate.now().getDayOfMonth() % 10) {
+            case 1:
+                day += "st";
+                break;
+            case 2:
+                day += "nd";
+                break;
+            case 3:
+                day += "rd";
+                break;
+            default:
+                day += "th";
+
+        }
         dayLabel.setText(String.format("%s %s %s", day, month, year));
     }
 
