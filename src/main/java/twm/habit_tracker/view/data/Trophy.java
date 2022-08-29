253d31fc1cc55ad19bc9b4ea95a25f74e5170401
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-08-29 21:19:47 +0100

    Keep implementing trophy class

--- src/main/java/twm/habit_tracker/view/data/Trophy.java
+++ src/main/java/twm/habit_tracker/view/data/Trophy.java
@@ -1,8 +1,30 @@
 package twm.habit_tracker.view.data;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+
 public class Trophy extends ModelData{
+    private BooleanProperty trophyWon = new SimpleBooleanProperty();
+
+    public boolean isTrophyWon() {
+        return trophyWon.get();
+    }
+
+    public BooleanProperty trophyWonProperty() {
+        return trophyWon;
+    }
+
+    public void setTrophyWon(boolean trophyWon) {
+        this.trophyWon.set(trophyWon);
+    }
+
     @Override
     public String[] getAllFields() {
-        return new String[0];
+        String[] output = {
+                getPrimaryKey(),
+                String.valueOf(isTrophyWon())
+        } ;
+        return output;
     }
 }
