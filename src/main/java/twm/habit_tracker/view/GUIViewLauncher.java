dc526eefa28b1113a9960b639f94b81e9d549f0a
Tobias Masters <tmaste02@mail.bbk.ac.uk>
2022-09-15 21:45:20 +0100

    Update notes in ViewLauncher

--- src/main/java/twm/habit_tracker/view/GUIViewLauncher.java
+++ src/main/java/twm/habit_tracker/view/GUIViewLauncher.java
@@ -1,45 +1,31 @@
 package twm.habit_tracker.view;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.HashMap;
 
 /**
  * Class for launching app window and setting window variables
  */
 public class GUIViewLauncher implements ViewLauncher {
-    private static String CSS = Path.of("src/main/resources/twm/habit_tracker/view/stylesheet.css").toUri().toString();
-
     private Parent menu_page;
-    private HashMap<String, Node> pages = new HashMap<>();
-
-
-    @Override
-    public Node getPage(String pageID) {
-        return pages.get(pageID);
-    }
 
     @Override
     public void setUp(Stage primaryStage) {
         primaryStage.initStyle(StageStyle.DECORATED);
 
-        // Load Pages!
         try {
             menu_page = FXMLLoader.load(GUIViewLauncher.class.getResource("MenuPage.fxml"));
         } catch (IOException e) {
             System.out.println("Problems when loading Menu FXML File");
             e.printStackTrace();
         }
-
         Scene scene = new Scene(menu_page, 1000, 1200); // Width, then Height
-        scene.getStylesheets().add(CSS);
+        scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 
         primaryStage.setScene(scene);
         primaryStage.show();
--- src/main/java/twm/habit_tracker/view/ViewLauncher.java
+++ src/main/java/twm/habit_tracker/view/ViewLauncher.java
@@ -1,16 +1,8 @@
 package twm.habit_tracker.view;
 
-import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public interface ViewLauncher {
-
-    /**
-     * Return page based on ID
-     * @param pageID
-     * @return Node
-     */
-    Node getPage(String pageID);
 
     /**
      * Set up stage at start
